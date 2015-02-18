75e090545cb78028ed68a76650d48dc563fe7d97
Юзер <@192.168.1.11>
2015-02-18 17:13:00 +0200

    Intelictual  not ignor.

---
+++ src/main/java/iis/test1/App.java
@@ -0,0 +1,14 @@
+package iis.test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello!" );
+    }
+}
---
+++ src/test/java/iis/test1/AppTest.java
@@ -0,0 +1,12 @@
+package iis.test1;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    public void testApp()
+    {
+        System.out.println("Hello my");
+    }
+}
